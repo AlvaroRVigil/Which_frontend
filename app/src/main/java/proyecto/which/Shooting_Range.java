6196e5e83cd0f8c3d5f2f3f018ed134ee75631c7
alvaro <alvaro@alvaro.com>
2017-03-08 17:46:08 +0100

    Initial commit

---
+++ app/src/main/java/proyecto/which/Shooting_Range.java
@@ -0,0 +1,13 @@
+package proyecto.which;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Shooting_Range extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shooting_range);
+    }
+}

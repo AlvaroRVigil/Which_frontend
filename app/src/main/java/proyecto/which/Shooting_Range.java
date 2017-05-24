8daf744264051e7a2630d6ca3289d2132b04e707
AlvaroRVigil <alvaro.rodriguez.vigil@gmail.com>
2017-05-24 17:31:03 +0200

    arreglos manifest y strings

--- app/src/main/java/proyecto/which/Shooting_Range.java
+++ app/src/main/java/proyecto/which/Shooting_Range.java
@@ -1,88 +1,78 @@
 package proyecto.which;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class Shooting_Range extends AppCompatActivity {
-
-    EditText etDato;
-    Button btnGuardar, btnMostrar;
+public class Shooting_Range extends Activity {
+    private EditText textBox;
+    static final int READ_BLOCK_SIZE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_shooting_range);
 
-        final Context context = this;
-        final SharedPreferences sharprefs = getSharedPreferences("ArchivoSP", context.MODE_PRIVATE);
+        textBox = (EditText) findViewById(R.id.txtText1);
+    }
 
+    public void onClickGuardar(View v){
+        String str = textBox.getText().toString();
+        try{
+            FileOutputStream fos = openFileOutput("textFile.txt", MODE_PRIVATE);
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
 
-        etDato = (EditText)findViewById(R.id.etDato);
-        btnMostrar = (Button)findViewById(R.id.btnMostrar);
-        btnGuardar = (Button)findViewById(R.id.btnGuardar);
+            // Escribimos el String en el archivo
+            osw.write(str);
+            osw.flush();
+            osw.close();
 
-        btnMostrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SharedPreferences sharpref = getPreferences(context.MODE_PRIVATE);
-                String valor = sharpref.getString("MiDato","No hay dato");
-                Toast.makeText(getApplicationContext(), "Dato guardado : "+valor,Toast.LENGTH_LONG).show();
+            // Mostramos que se ha guardado
+            Toast.makeText(getBaseContext(), "Guardado", Toast.LENGTH_SHORT).show();
 
-            }
-        });
-
-        btnGuardar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                SharedPreferences sharpref = getPreferences(context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharpref.edit();
-                editor.putString("MiDato", etDato.getText().toString());
-                editor.commit();
-
-            }
-        });
-
-
-        /*
-        Context context = this;
-        SharedPreferences sharpref = context.getSharedPreferences("ArchivoSP",context.MODE_PRIVATE);
-
-
-        etDato = (EditText)findViewById(R.id.etDato);
-        btnGuardar = (Button)findViewById(R.id.btnGuardar);
-        btnMostrar = (Button)findViewById(R.id.btnMostrar);
-
-        btnGuardar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPref.edit();
-                editor.putString("DatoGuardado", etDato.getText().toString());
-
-                editor.commit();
-
-            }
-        });
-
-        btnMostrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-                String Valor = sharedPref.getString("DatoGuardado", "sin dato");
-                Toast.makeText(getApplicationContext(), "Valor guardado : "+Valor, Toast.LENGTH_LONG).show();
-            }
-        });*/
-
+            textBox.setText("");
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
 
     }
+
+    public void onClickCargar(View v){
+        try{
+            FileInputStream fis = openFileInput("textFile.txt");
+            InputStreamReader isr = new InputStreamReader(fis);
+
+            char[] inputBuffer = new char[READ_BLOCK_SIZE];
+            String s = "";
+
+            int charRead;
+            while((charRead = isr.read(inputBuffer)) > 0){
+                // Convertimos los char a String
+                String readString = String.copyValueOf(inputBuffer, 0, charRead);
+                s += readString;
+
+                inputBuffer = new char[READ_BLOCK_SIZE];
+            }
+
+            // Establecemos en el EditText el texto que hemos leido
+            textBox.setText(s);
+
+            // Mostramos un Toast con el proceso completado
+            Toast.makeText(getBaseContext(), "Cargado", Toast.LENGTH_SHORT).show();
+
+            isr.close();
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+
 }

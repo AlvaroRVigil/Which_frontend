bca2af7d5c40136a234a1ea0a9f5cd91025c7d5e
AlvaroRVigil <alvaro.rodriguez.vigil@gmail.com>
2017-05-30 18:46:13 +0200

    Pantallas filtro marcas y sistemas definitivo / barra lateral definitiva / top lista definitivo / cambios en la interfaz ( finalizada un 90%)

--- app/src/main/java/proyecto/which/activities/MarcasActivity.java
+++ app/src/main/java/proyecto/which/activities/MarcasActivity.java
@@ -2,7 +2,9 @@ package proyecto.which.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -121,14 +123,14 @@ public class MarcasActivity extends AppCompatActivity
 
 
 
-      /*  FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+      FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabBuscar);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent devi = new Intent(MarcasActivity.this, BuscarActivity.class);
+                startActivity(devi);
             }
-        }); */
+        });
 
 
 

183c89561a4fc22784745e1f03b17679442c1bc8
AlvaroRVigil <alvaro.rodriguez.vigil@gmail.com>
2017-06-07 10:21:36 +0200

    Contacto final

--- app/src/main/java/proyecto/which/activities/ContactActivity.java
+++ app/src/main/java/proyecto/which/activities/ContactActivity.java
@@ -1,15 +1,66 @@
 package proyecto.which.activities;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import java.util.HashMap;
 
 import proyecto.which.R;
 
 public class ContactActivity extends AppCompatActivity {
 
+    private ImageButton contacto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        ImageButton back = (ImageButton) findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+
+                Intent Intentback = new Intent(ContactActivity.this, MainActivity.class);
+                startActivity(Intentback);
+
+            }
+
+
+        });
+
+        contacto =(ImageButton)findViewById(R.id.contacto);
+        contacto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] to = { "whichapp.contact@gmail.com"};
+                String[] cc = { "" };
+                enviar(to, cc, "",
+                        "");
+
+            }
+
+        });
     }
+
+    private void enviar(String[] to, String[] cc, String asunto, String mensaje) {
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, to);
+        emailIntent.putExtra(Intent.EXTRA_CC, cc);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, asunto);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, mensaje);
+        emailIntent.setType ("message/rfc822");
+        startActivity(Intent.createChooser(emailIntent, "Email "));
+
+    }
+    
+
 }
+
